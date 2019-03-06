7c5103d66965395fa64148868bc301cd6b182b7f
asd <asd@asd.asd>
2019-03-06 23:19:35 +0100

    hiba

--- js/content.js
+++ js/content.js
@@ -1,30 +1,52 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const url = require("url");
 class Content {
     content(req, res) {
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.write("<!DOCTYPE html>");
+        res.write('<html lang="hu"><head>'
+            + '<meta name="viewport" content="width=device-width, initial-scale=1.0">'
+            + '<meta http-equiv="X-UA-Compatible" content="ie=edge">'
+            + "<title>Házi</title></head>");
+        res.write("<body>");
+        const query = url.parse(req.url, true).query;
+        const xKezd = query.x === undefined ? "1" : query.x;
+        const a = query.a === undefined ? 1 : parseInt(query.a, 10);
+        const b = query.b === undefined ? 5 : parseInt(query.b, 10);
+        let x = parseInt(xKezd, 10);
         res.write("<h1>Szelekció (elágazás)</h1>");
         res.write("<h3>Az egyágú elágazás</h3>");
-        let x = -3.14;
+        // res.write("<p>x értéke = " +
+        //     "<input type = 'number' name='x' " +
+        //     "style= 'font-family:Courier; font-size: inherit; background-color: LightBlue;'" +
+        //     `value='${x}' onChange = 'this.form.submit();'><br>`);
+        res.write('<input type="text" name="x" value="1" onChange = "this.form.submit()"><br>');
         res.write("Szám abszolút értéke<br>"
-            + `x = ${x}<br>`);
-        if (x < 0)
+            + `</p>`);
+        if (x < 0) {
             x = -x;
+        }
         res.write(`|x| = ${x}`);
         res.write("<h3>A kétágú elágazás</h3>");
-        const a = 5;
-        res.write("Páros-Páratlan meghatározó<br>"
-            + `x = ${a}<br>`);
-        if (x % 2 === 0) {
+        res.write("Páros-Páratlan meghatározó<br>");
+        res.write("<p>a értéke = " +
+            "<input type = 'number' name= 'a' " +
+            "style= 'font-family:Courier; font-size: inherit; background-color: LightBlue;'" +
+            `value='${a}' onChange = 'this.form.submit();'><br>` + `</p>`);
+        if (a % 2 === 0) {
             res.write("A szám páros");
         }
         else {
             res.write("A szám páratlan");
         }
         res.write("<h3>A többágú elágazás</h3>");
-        const b = 3;
+        res.write("<p>b értéke = " +
+            "<input type = 'number' name= 'b' " +
+            "style= 'font-family:Courier; font-size: inherit; background-color: LightBlue;'" +
+            `value='${b}' onChange = 'this.form.submit();'><br>`);
         res.write("Osztályzat<br>"
-            + `x = ${b}<br>`);
+            + "</p>");
         switch (b) {
             case 1:
                 res.write("Elégtelen<br>");
@@ -45,6 +67,7 @@ class Content {
                 res.write("Nem osztályzat!<br>");
                 break;
         }
+        res.end("</body></html>");
         res.end();
     }
 }
--- ts/content.ts
+++ ts/content.ts
@@ -1,29 +1,52 @@
 ﻿import * as fs from "fs";
 import * as http from "http";
+import { ParsedUrlQuery } from "querystring";
+import * as url from "url";
 
 export default class Content {
 
     public content(req: http.IncomingMessage, res: http.ServerResponse): void {
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
 
+        res.write("<!DOCTYPE html>");
+        res.write('<html lang="hu"><head>'
+            + '<meta name="viewport" content="width=device-width, initial-scale=1.0">'
+            + '<meta http-equiv="X-UA-Compatible" content="ie=edge">'
+            + "<title>Házi</title></head>");
+        res.write("<body>");
+        const query: ParsedUrlQuery = url.parse(req.url, true).query;
+        const xKezd: string = query.x === undefined ? "1" : (query.x as string);
+        const a: number = query.a === undefined ? 1 : parseInt((query.a as string), 10);
+        const b: number = query.b === undefined ? 5 : parseInt((query.b as string), 10);
+
+        let x: number = parseInt(xKezd, 10);
         res.write("<h1>Szelekció (elágazás)</h1>");
         res.write("<h3>Az egyágú elágazás</h3>");
-        let x: number = -3.14;
+        // res.write("<p>x értéke = " +
+        //     "<input type = 'number' name='x' " +
+        //     "style= 'font-family:Courier; font-size: inherit; background-color: LightBlue;'" +
+        //     `value='${x}' onChange = 'this.form.submit();'><br>`);
+        res.write('<input type="text" name="x" value="1" onChange = "this.form.submit()"><br>');
         res.write("Szám abszolút értéke<br>"
-            + `x = ${x}<br>`);
-        if (x < 0) x = -x;
+            + `</p>`);
+        if (x < 0) { x = -x }
         res.write(`|x| = ${x}`);
 
         res.write("<h3>A kétágú elágazás</h3>");
-        const a: number = 5;
-        res.write("Páros-Páratlan meghatározó<br>"
-            + `x = ${a}<br>`);
-        if (x % 2 === 0) { res.write("A szám páros"); } else { res.write("A szám páratlan"); }
+        res.write("Páros-Páratlan meghatározó<br>");
+        res.write("<p>a értéke = " +
+            "<input type = 'number' name= 'a' " +
+            "style= 'font-family:Courier; font-size: inherit; background-color: LightBlue;'" +
+            `value='${a}' onChange = 'this.form.submit();'><br>` + `</p>`);
+        if (a % 2 === 0) { res.write("A szám páros"); } else { res.write("A szám páratlan"); }
 
         res.write("<h3>A többágú elágazás</h3>");
-        const b: number = 3;
+        res.write("<p>b értéke = " +
+            "<input type = 'number' name= 'b' " +
+            "style= 'font-family:Courier; font-size: inherit; background-color: LightBlue;'" +
+            `value='${b}' onChange = 'this.form.submit();'><br>`);
         res.write("Osztályzat<br>"
-            + `x = ${b}<br>`);
+            + "</p>");
         switch (b) {
             case 1: res.write("Elégtelen<br>"); break;
             case 2: res.write("Elégséges<br>"); break;
@@ -32,7 +55,7 @@ export default class Content {
             case 5: res.write("Jeles<br>"); break;
             default: res.write("Nem osztályzat!<br>"); break;
         }
-
+        res.end("</body></html>");
         res.end();
     }
 }
